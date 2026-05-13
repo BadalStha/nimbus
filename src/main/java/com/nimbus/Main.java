5e87f2ab7555e339423ed29d1e5c79968ca53166
Badal Shrestha <shresthabadal324@gmail.com>
2026-05-13 19:21:50 +0545

    feat: implement search and filetering using Java Streams API

--- src/main/java/com/nimbus/Main.java
+++ src/main/java/com/nimbus/Main.java
@@ -1,6 +1,7 @@
 package com.nimbus;
 
 import java.util.List;
+import java.util.stream.Collectors;
 import java.util.stream.StreamSupport;
 import java.util.ArrayList;
 
@@ -36,12 +37,15 @@ public class Main {
 
         Song song1 = new Song ("Highway to Hell", "AC/DC", 328, 320);
 
-        LocalSong song2 = new LocalSong("Timi Bhane", "Albatross", 503, 320,"/home/badal/Music/");
+
+        LocalSong song2 = new LocalSong("Timi Bhane", "Albatross", 503, 320,"/home/badal/Music/timibhane.mp3");
+        LocalSong song4 = new LocalSong("Laijane Ho Ki", "Yankee Yolmo", 316, 320, "/home/badal/Music/Laijanehoki.mp3");
         CloudSong song3 = new CloudSong("Tum Tak", "Javed Ali", 505, 320, "examplelinktomucic.com");
 
         nimbusPlaylist.add(0, song1);
         nimbusPlaylist.add(song2);
         nimbusPlaylist.add(song3);
+        nimbusPlaylist.add(song4);
 
         try {
             emptyPlayList.get(0);
@@ -61,6 +65,30 @@ public class Main {
         }
 
         System.out.println("Total Songs in Library: " + nimbusPlaylist.size());
+
+        // Search song with artist
+        System.out.println("\n---Searching for 'Swar' ---");
+
+        List<Song> searchResults = nimbusPlaylist.stream()
+                .filter(s -> s.artist.equalsIgnoreCase("Swar"))
+                .collect(Collectors.toList());
+
+        searchResults.forEach(s -> System.out.println("Found: " + s.title));
+
+        // Search song with duration
+        System.out.println("\n--- Searching for duration ---");
+        List<Song> durationResults = nimbusPlaylist.stream()
+                .filter(s -> s.duration > 300)
+                .collect(Collectors.toList());
+
+        durationResults.forEach(s -> System.out.println("Found: " + s.title));
+
+        // Find total local song
+        long totalLocal = nimbusPlaylist.stream()
+                .filter(s -> s instanceof LocalSong).count();
+
+        System.out.println("Total Local Songs: " + totalLocal);
+
 
     }
 
